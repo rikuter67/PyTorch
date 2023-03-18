334e23ef7796820ae6e307172413153157c0e49c
rikuter67 <rikuto.01.05.04@gmail.com>
2023-03-18 15:05:23 +0900

    23/03/18/15:05

---
+++ 4/6_RandomForestClassifer.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',header=None)
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total Phenols', 'Flavanoids',
+                   'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines', 'Proline']
+from sklearn.model_selection import train_test_split
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0, stratify=y)
+
+# scikit-learnの場合
+from sklearn.preprocessing import StandardScaler
+# 標準化のインスタンスを生成（平均＝０、標準偏差＝１に変換）
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+
+
+from sklearn.ensemble import RandomForestClassifier
+# Wineデータセットの特徴量の名称
+feat_labels = df_wine.columns[1:]
+# ランダムフォレストオブジェクトの生成(決定木の個数=500)
+forest = RandomForestClassifier(n_estimators=500, random_state=1)
+# モデルを適合
+forest.fit(X_train, y_train)
+# 特徴量の重要度を抽出
+importances = forest.feature_importances_
+# 重要度の降順で特徴量のインデックスを抽出
+indices = np.argsort(importances)[::-1]
+# 重要度の降順で特徴量の名称、重要度を表示
+for f in range(X_train.shape[1]):
+    print("%2d) %-*s %f" %
+          (f + 1, 30, feat_labels[indices[f]], importances[indices[f]]))
+    
+plt.title('Feature importances')
+plt.bar(range(X_train.shape[1]), importances[indices], align='center')
+plt.xticks(range(X_train.shape[1]), feat_labels[indices], rotation=90)
+plt.xlim([-1, X_train.shape[1]])
+plt.tight_layout()
+plt.show()

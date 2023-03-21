ae9beac11923e5f879166e661f3d5a987e2ecebe
rikuter67 <rikuto.01.05.04@gmail.com>
2023-03-21 12:17:39 +0900

    23/03/31

--- 5/1_PCA.py
+++ 5/1_PCA.py
@@ -49,15 +49,97 @@ w = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis
 
 X_train_pca = X_train_std.dot(w)
 
-colors = ['r', 'b', 'g']
-markers = ['o', 's', '^']
-# 「クラスラベル」「点の色」「点の種類」の組み合わせからなるリストを作成してプロット
-for l, c, m in zip(np.unique(y_train), colors, markers):
-    plt.scatter(X_train_pca[y_train==l, 0], X_train_pca[y_train==l, 1],
-                c=c, label=f'Class {l}', marker=m)
+# colors = ['r', 'b', 'g']
+# markers = ['o', 's', '^']
+# # 「クラスラベル」「点の色」「点の種類」の組み合わせからなるリストを作成してプロット
+# for l, c, m in zip(np.unique(y_train), colors, markers):
+#     plt.scatter(X_train_pca[y_train==l, 0], X_train_pca[y_train==l, 1],
+#                 c=c, label=f'Class {l}', marker=m)
     
+# plt.xlabel('PC 1')
+# plt.ylabel('PC 2')
+# plt.legend(loc='lower left')
+# plt.tight_layout()
+# plt.show()
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    # マーカーとカラーマップの準備
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    # 決定領域のプロット
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    # グリッドポイントの生成
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    # 各特徴量を１次元配列に変換して予測を実行
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    # 予測結果を元のグリッドポイントのデータサイズに変換
+    lab = lab.reshape(xx1.shape)
+    # グリッドポイントの等高線のプロット
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    # 軸の範囲の設定
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    # クラスごとにデータ点をプロット
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=colors[idx],
+                    marker=markers[idx], label=f'Class {cl}', edgecolors='black')
+        
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+# 主成分数を指定して、PCAのインスタンスを生成
+pca = PCA(n_components=2)
+# ロジスティック回帰のインスタンスを生成
+lr = LogisticRegression(multi_class='ovr', random_state=1, solver='lbfgs')
+# 次元削減
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+# 削減したデータセットでロジスティック回帰モデルを適合
+lr.fit(X_train_pca, y_train)
+
+# 決定境界をプロット(train)
+# plot_decision_regions(X_train_pca, y_train, classifier=lr)
+# plt.xlabel('PC 1')
+# plt.ylabel('PC 2')
+# plt.legend(loc='lower left')
+# plt.tight_layout()
+# plt.show()
+
+# 決定境界をプロット(test)
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
 plt.xlabel('PC 1')
 plt.ylabel('PC 2')
 plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+pca = PCA(n_components=None)
+X_train_pca = pca.fit_transform(X_train_std)
+# 分散説明率を計算
+print(pca.explained_variance_ratio_)
+
+loadings = eigen_vecs * np.sqrt(eigen_vals)
+
+fig, ax = plt.subplots()
+ax.bar(range(13), loadings[:, 0], align='center')
+ax.set_ylabel('Loadings for PC 1')
+ax.set_xticks(range(13))
+ax.set_xticklabels(df_wine.columns[1:], rotation=90)
+plt.ylim([-1, 1])
+plt.tight_layout()
+plt.show()
+
+sklearn_loadings = pca.components_.T * np.sqrt(eigen_vals)
+
+fig, ax = plt.subplots()
+ax.bar(range(13), sklearn_loadings[:, 0],align='center')
+ax.set_ylabel('Loadings for PC 1')
+ax.set_xticks(range(13))
+ax.set_xticklabels(df_wine.columns[1:], rotation=90)
+plt.ylim([-1, 1])
 plt.tight_layout()
 plt.show()

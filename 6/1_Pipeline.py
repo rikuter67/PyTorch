53b699532e55b30b89905df2ac876205315ae23b
rikuter67 <rikuto.01.05.04@gmail.com>
2023-03-23 13:18:21 +0900

    23/03/23/13:18

---
+++ 6/1_Pipeline.py
@@ -0,0 +1,31 @@
+import pandas as pd
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/'
+                 'breast-cancer-wisconsin/wdbc.data', header=None)
+
+from sklearn.preprocessing import LabelEncoder
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+# print(le.classes_)
+# print(le.transform(['M', 'B']))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, stratify=y, random_state=1)
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+# 連結する処理としてスケーリング、主成分分析、ロジスティック回帰を指定
+pipe_lr = make_pipeline(StandardScaler(),
+                       PCA(n_components = 2),
+                       LogisticRegression())
+pipe_lr.fit(X_train, y_train)
+y_pred = pipe_lr.predict(X_test)
+test_acc = pipe_lr.score(X_test, y_test)
+print(f'Test accuracy: {test_acc:.3f}')
+
+
+
+

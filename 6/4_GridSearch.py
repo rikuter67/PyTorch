27571bebc9bd0b558b9f2180294e7a31bce2c1f9
rikuter67 <rikuto.01.05.04@gmail.com>
2023-03-25 01:25:24 +0900

    23/03/25/1:25

--- 6/4_GridSearch.py
+++ 6/4_GridSearch.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import scipy
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/'
                  'breast-cancer-wisconsin/wdbc.data', header=None)
 
@@ -37,3 +38,67 @@ print(gs.best_score_)
 
 # 最良スコアとなるパラメータ値を出力
 print(gs.best_params_)
+
+clf = gs.best_estimator_
+clf.fit(X_train, y_train)
+print(f'Test accuracy: {clf.score(X_test, y_test):.3f}')
+
+
+param_range = scipy.stats.loguniform(0.0001, 1000.0)
+
+np.random.seed(1)
+param_range.rvs(10)
+
+from sklearn.model_selection import RandomizedSearchCV
+pipe_svc = make_pipeline(StandardScaler(), SVC(random_state=1))
+param_grid = [{'svc__C': param_range, 'svc__kernel': ['linear']},
+              {'svc__C': param_range, 'svc__gamma': param_range,
+               'svc__kernel': ['rbf']}]
+rs = RandomizedSearchCV(estimator=pipe_svc, param_distributions=param_grid,
+                        scoring='accuracy', refit=True, n_iter=20, cv=10,
+                        random_state=1, n_jobs=-1)
+rs = rs.fit(X_train, y_train)
+print(rs.best_params_)
+
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingRandomSearchCV
+hs = HalvingRandomSearchCV(
+    pipe_svc,
+    param_distributions=param_grid,
+    n_candidates='exhaust',
+    resource='n_samples',
+    factor=1.5,
+    random_state=1,
+    n_jobs=-1)
+
+hs = hs.fit(X_train, y_train)
+print(hs.best_score_)
+
+print(hs.best_params_)
+
+clf = hs.best_estimator_
+print(f'Test accuracy: {hs.score(X_test, y_test):.3f}')
+
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+param_grid = [{'svc__C': param_range, 'svc__kernel': ['linear']},
+              {'svc__C': param_range, 'svc__gamma': param_range,
+               'svc__kernel': ['rbf']}]
+gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid,
+                  scoring='accuracy', cv=2)
+
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
+print(f'CV accuracy: {np.mean(scores):.3f}'
+      f'+/- {np.std(scores):.3f}')
+
+from sklearn.tree import DecisionTreeClassifier
+# ハイパーパラメータ値として決定木の深さパラメータを指定し、
+# グリッドサーチを行うGridSearchCVクラスをインスタンス化
+gs = GridSearchCV(estimator=DecisionTreeClassifier(random_state=0),
+                  param_grid=[{'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}],
+                  scoring='accuracy', cv=2)
+scores = cross_val_score(gs,
+                         X_train, y_train,
+                         scoring='accuracy', cv=5)
+print(f'CV accuracy: {np.mean(scores):.3f}'
+      f'+/- {np.std(scores):.3f}')

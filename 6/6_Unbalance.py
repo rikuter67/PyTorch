9b495a1a737de71b51e435e7ebc5f0d493dac6c4
rikuter67 <rikuto.01.05.04@gmail.com>
2023-03-27 03:04:16 +0900

    23/03/27/3:04

---
+++ 6/6_Unbalance.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/'
+                 'breast-cancer-wisconsin/wdbc.data', header=None)
+
+from sklearn.preprocessing import LabelEncoder
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_imb = np.vstack((X[y == 0], X[y == 1][:40]))
+y_imb = np.hstack((y[y == 0], y[y == 1][:40]))
+
+y_pred = np.zeros(y_imb.shape[0])
+print(np.mean(y_pred == y_imb) * 100)
+
+from sklearn.utils import resample
+print('Number of class 1 examples before:', X_imb[y_imb == 1].shape[0])
+
+# データ点の個数がクラス0と同じになるまで新しいデータ点を復元抽出
+X_upsampled, y_upsampled = resample(X_imb[y_imb == 1],
+                                    y_imb[y_imb == 1],
+                                    replace=True,
+                                    n_samples=X_imb[y_imb == 0].shape[0],
+                                    random_state=123)
+print('Number of class 1 examples after:', X_upsampled.shape[0])
+
+X_bal = np.vstack((X[y == 0], X_upsampled))
+y_bal = np.hstack((y[y == 0], y_upsampled))
+
+y_pred = np.zeros(y_bal.shape[0])
+print(np.mean(y_pred == y_bal) * 100)
